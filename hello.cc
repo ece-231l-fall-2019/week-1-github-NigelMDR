ce8b6a65707aff6cfb03f7d2e79c00d16424132b
NigelMDR <54381052+NigelMDR@users.noreply.github.com>
2019-09-01 10:18:32 -0600

    Week 1 - GitHub

---
+++ hello.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+
+	std::cout << " ## HISTORY NIGEL "<<std::endl << std::endl;
+	std::cout << " Hello my name is Nigel Damian Reyes I was born in peru and moved to the US when I was seven years old." << std::endl; 
+	std::cout << " My goal for this call is just have fun and along the way learn something new. "<<std::endl;
+       	std::cout << " My hope is that in the future I'm a part time photographer and a full time inventor." << std::endl;
+	std::cout << "## Interesting fact about me"<<std::endl << std::endl;
+	std::cout << "Likes Dogs"<<std::endl << std::endl;
+	std::cout << "**Likes Photos"<<std::endl << std::endl;
+	std::cout << "*Nature"<<std::endl << std::endl;
+	std::cout << "That all"<<std::endl << std::endl;
+	std::cout << "#** Good By"<<std::endl << std::endl;
+
+	std::cout << "- [x] PIZZA." << std::endl;
+        std::cout << "- [] CAT" << std::endl;
+        std::cout << "- [x] Nature." << std::endl;
+
+	std::cout << "#** Good By"<<std::endl << std::endl;
+	std::cout << "#** Good By"<<std::endl << std::endl;
+
+
+	return 0;
+
+}
---
+++ hello.md
@@ -0,0 +1,31 @@
+ ## HISTORY NIGEL 
+
+ Hello my name is Nigel Damian Reyes I was born in peru and moved to the US when I was seven years old.
+ My goal for this call is just have fun and along the way learn something new. 
+ My hope is that in the future I'm a part time photographer and a full time inventor.
+## Interesting fact about me
+
+Likes Dogs
+
+**Likes Photos
+
+*Nature
+
+That all
+
+#** Good By
+
+![Deer](https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1007655361%2F960x0.jpg%3Ffit%3Dscale)
+
+ :deer: 'Nice Deer':deer:
+
+- [x] PIZZA. :pizza:
+- [] CAT :cat:
+- [x] Nature. :dog:
+#** Good By
+
+"Bad Cat"
+
+#* Good By
+
+#### :Dot: :Cat:
